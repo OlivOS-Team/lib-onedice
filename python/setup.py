ac03ae2cf8f680ec9614e0d9635bd676be205e3f
仑质 <benzenpenxil@foxmail.com>
2022-11-25 10:08:56 +0800

    pypi版本推进至1.0.5

--- python/setup.py
+++ python/setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="onedice",
-    version="1.0.4",
+    version="1.0.5",
     author="lunzhiPenxil",
     author_email="lunzhiPenxil@gmail.com",
     description="OneDice offical standard library for Python",
